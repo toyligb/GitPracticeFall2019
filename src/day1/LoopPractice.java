a66e936adf2fe9c01f8cca3a540ce7391de4ed17
Toyly Gurdov <toyligb@gmail.com>
2020-02-29 12:51:56 -0800

    modified

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -3,7 +3,7 @@ package day1;
 public class LoopPractice {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Counter : " + i);
+            System.out.println("Counter : " + (i+1));
         }
     }
 }
