b927a6123a367303fedb8b15e5ec1de328924107
Toyly Gurdov <toyligb@gmail.com>
2020-02-29 12:24:53 -0800

    Modified StringPractice

--- src/day1/StringPractice.java
+++ src/day1/StringPractice.java
@@ -5,6 +5,8 @@ public class StringPractice {
     public static void main(String[] args) {
 
         System.out.println("\"Hello World!\" = " + "Hello World!");
+        String str = "Java is fun";
+        System.out.println("str = " + str);
 
     }
 
